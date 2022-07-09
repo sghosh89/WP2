3750a498e54c16ceccbe0edf54becc5261141e0a
sghosh89 <ghoshshyamolina89@gmail.com>
2022-07-09 12:00:53 +0200

    edits

--- R/get_unique_lonlat.R
+++ R/get_unique_lonlat.R
@@ -2,14 +2,11 @@ rm(list=ls())
 library(tidyverse)
 ##########################
 # first read all the metadata collected for proj BioDyn
-s<-read.csv("../Results/metadata_summary_from_BioDyn.csv")
-
-# get first unique set of long+lat combination
-s_lonlat<-s%>%select(CENT_LONG,CENT_LAT)
-s_lonlat <- s_lonlat[!duplicated(s_lonlat[ , c("CENT_LONG", "CENT_LAT")]), ] 
-rownames(s_lonlat)<-NULL
-nrow(s_lonlat) # 1891 unique lon+lat combo
-
-write.csv(s_lonlat,"../Results/unique_lonlat.csv",row.names = F)
-#######################################
-
+md<-read.csv("../Results/metadata_summary_from_BioDyn.csv")
+#========== get first unique set of long+lat combination for all metadata ===========
+md_lonlat<-md%>%select(CENT_LONG,CENT_LAT)
+md_lonlat <- md_lonlat[!duplicated(md_lonlat[ , c("CENT_LONG", "CENT_LAT")]), ] 
+rownames(md_lonlat)<-NULL
+nrow(md_lonlat) 
+write.csv(md_lonlat,"../Results/unique_lonlat.csv",row.names = F)
+#===============================================================================
