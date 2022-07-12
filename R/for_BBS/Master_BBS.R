c015bfcb42ffbd7345dae8d9187c3e7205e65890
sghosh89 <ghoshshyamolina89@gmail.com>
2022-07-12 09:04:26 +0200

    delete

--- R/for_BBS/Master_BBS.R
+++
@@ -1,7 +0,0 @@
-path<-dirname(rstudioapi::getSourceEditorContext()$path)
-setwd(path)
-
-# wrangling data, find good routes, get community matrix minimum for 20 years, 
-# species included sampled for at least 70% of sampling period, sp_threshold=4
-source("data_wrangling.R") 
-#source("BBS_ta.R")         # tail analysis for each route
