44ad049db6545e7a6dc6aae7649fd266ab58f5ad
sghosh89 <ghoshshyamolina89@gmail.com>
2022-07-19 15:13:06 +0200

    edits

--- R/get_unique_lonlat.R
+++ R/get_unique_lonlat.R
@@ -1,9 +1,10 @@
+library(dplyr)
 library(tidyverse)
 ##########################
 # first read all the metadata collected for proj BioDyn
 md<-read.csv("../DATA/metadata_summary.csv")
 #========== get first unique set of long+lat combination for all metadata ===========
-md_lonlat<-md%>%select(CENT_LONG,CENT_LAT)
+md_lonlat<-md%>%dplyr::select(CENT_LONG,CENT_LAT)
 md_lonlat <- md_lonlat[!duplicated(md_lonlat[ , c("CENT_LONG", "CENT_LAT")]), ] 
 rownames(md_lonlat)<-NULL
 nrow(md_lonlat) 
