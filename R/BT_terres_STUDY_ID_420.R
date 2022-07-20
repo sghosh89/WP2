edf800d39675bf2138d67f3689b93f18f8f57c5d
sghosh89 <ghoshshyamolina89@gmail.com>
2022-07-20 17:43:00 +0200

    edits

--- R/BT_terres_STUDY_ID_420.R
+++ R/BT_terres_STUDY_ID_420.R
@@ -8,9 +8,12 @@ library(tidyverse)
 #metadata_BT<-readRDS("../DATA/for_BioTIME/BioTIME_public_private_metadata.RDS")
 #grid_terres<-readRDS("../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/bt_terres_min20yr_rawdata.RDS")
 #terres_tbl_for_map<-readRDS("../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/table_for_map.RDS")
-#env_BT<-read.csv("../DATA/for_BioTIME/wrangled_data/annual_tas_CHELSA_1979_2019_BT_lonlat.csv")
+#env_BT_t<-read.csv("../DATA/for_BioTIME/wrangled_data/annual_tas_CHELSA_1979_2019_BioTIME_lonlat.csv")
 
 df<-terres_tbl_for_map%>%filter(STUDY_ID==420)
+#df$newsite<-df$STUDY_ID # this is the same as there is single site
+
+
 #----------- create result folder for wrangle ddata -------------------------
 resloc<-"../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/420/"
 if(!dir.exists(resloc)){
@@ -19,7 +22,6 @@ if(!dir.exists(resloc)){
 #--------------------------------------------------------------------------------
 site<-df$STUDY_ID
 x<-grid_terres%>%filter(STUDY_ID==site)
-#x<-x%>%mutate(newsite=paste("STUDY_ID_",site,"_LON",LONGITUDE,"_LAT",LATITUDE,sep=""))
 x<-x%>%mutate(newsite=paste("STUDY_ID_",site,"_LAT",LATITUDE,"_LON",LONGITUDE,sep=""))
 newsite<-sort(unique(x$newsite))
 
@@ -31,8 +33,11 @@ tt<-tt%>%filter(n>=20)
 
 #update
 x_allsite<- x %>% filter(newsite %in% tt$newsite)
-newsite<-tt$newsite
-#-------------------------------------------------------------------------------------------------------------
+newsite<-tt$newsite #only 2 sites 
+
+mylonlat<-data.frame(lonlat=paste(x_allsite$LONGITUDE,x_allsite$LATITUDE,sep="_"))
+mylonlat<-mylonlat%>%distinct(lonlat)
+
 # sometimes months have different multiple sampling dates within a year
 # so, take the average
 x_allsite$Abundance<-as.numeric(x_allsite$Abundance)
@@ -44,25 +49,25 @@ x_allsite<-x_allsite%>%group_by(newsite,YEAR,MONTH,Species)%>%
             LATITUDE=LATITUDE,
             LONGITUDE=LONGITUDE)%>%ungroup()
 # NOTE: ABUNDANCE TYPE should be kept as it is - if NA then keep NA
-
 # Now, create folder for all these newsite
-for(k in 1:length(newsite)){
-  resloc2<-paste("../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/420/",newsite[k],"/",sep="")
-  if(!dir.exists(resloc2)){
-    dir.create(resloc2)
+if(length(newsite)>1){
+  for(k in 1:length(newsite)){
+    resloc2<-paste("../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/420/",newsite[k],"/",sep="")
+    if(!dir.exists(resloc2)){
+      dir.create(resloc2)
+    }
   }
 }
 #------------------------------------------------------------
 newsite_bad<-c()
 
-
-#------------------------------------------------------------
+#----------------------------
 for(k in 1:length(newsite)){
   
   x<-x_allsite%>%filter(newsite==newsite[k])
   
   # do not consider these unknown sp into analysis
-  x<-x%>%filter(Species%notin%c("unspecifiable ","Unknown","Unknown rotifer", "Unknown rotifer2", "unknown ","Unknown "))
+  x<-x%>%filter(Species%notin%c("Unknown","Unknown "))
   
   t0<-x%>%group_by(YEAR)%>%summarise(nm=n_distinct(MONTH))%>%ungroup()
   #t1<-x%>%group_by(YEAR,MONTH)%>%summarise(nd=n_distinct(DAY))%>%ungroup()
@@ -103,9 +108,16 @@ for(k in 1:length(newsite)){
   rownames(xmat)<-year
   
   xmeta<-metadata_BT%>%filter(STUDY_ID==site)
+  xmeta$lonlat<-mylonlat$lonlat[k]
   
   input_sp<-list(spmat=xmat,meta=xmeta)
-  resloc<-paste("../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/420/",newsite[k],"/",sep="")
+  
+  if(length(newsite)>1){
+    resloc<-paste("../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/420/",newsite[k],"/",sep="")
+  }else{
+    resloc<-"../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/420/"
+  }
+  
   saveRDS(input_sp,paste(resloc,"allspecies_timeseries_and_metadata.RDS",sep=""))
   
   #----------- saving input spmat for tailanal ---------------------
@@ -114,17 +126,20 @@ for(k in 1:length(newsite)){
   presentyr<-apply(X=m$spmat,MARGIN=2,FUN=function(x){sum(x>0)})
   presentyr<-unname(presentyr)
   commonspid<-which(presentyr>=0.7*nrow(m$spmat)) # common sp = present minimum 70% of sampled year
+  
   if(length(commonspid)>0){
     m1<-m$spmat[,commonspid]
     m1<-as.data.frame(m1)
     input_tailanal<-m1
+    tot_target_sp<-ncol(input_tailanal)
   }else{
     input_tailanal<-NA
+    tot_target_sp<-NA
     cat("no common species are found \n")
   }
   saveRDS(input_tailanal,paste(resloc,"commonspecies_timeseries.RDS",sep=""))
-  tot_target_sp<-ncol(input_tailanal)
   saveRDS(tot_target_sp,paste(resloc,"tot_target_sp.RDS",sep="")) # nsp
+  
   #----------------- consider the matrix data only in between 1979 to 2019 period to match with env data------------------
   input_tailanal$yr<-as.integer(rownames(input_tailanal))
   input_tailanal<-input_tailanal%>%filter(yr%in%c(1979:2019))
@@ -133,39 +148,44 @@ for(k in 1:length(newsite)){
   #check again
   suffyr<-(nrow(input_tailanal)>=20)
   if(suffyr==T){
-  #-----------------------adding environmental variable in the matrix-----------------------------
-  tempdat<-env_BT%>%filter(STUDY_ID%in%site)%>%filter(yr%in%rownames(input_tailanal))%>%dplyr::select(yr,t,tmax,tmin)
-  tempdat$tmax_n<- -tempdat$tmax
-  
-  # check if all TRUE
-  all(rownames(input_tailanal)==tempdat$yr)==T
-  
-  input_tailanal$t<-tempdat$t
-  input_tailanal$tmax<-tempdat$tmax
-  input_tailanal$tmin<-tempdat$tmin
-  input_tailanal$tmax_n<-tempdat$tmax_n
-  input_tailanal<-input_tailanal%>%dplyr::select(-yr)
-  
-  saveRDS(input_tailanal,paste(resloc,"input_mat_for_tailanal_with_env.RDS",sep="")) # dataframe with species timeseries along column
-  
-  #----------------- now do tail analysis ----------------------
-  resloc2<-paste("../Results/for_BioTIME/Terrestrial_plotlevel/",site,"/",sep="")
-  if(!dir.exists(resloc2)){
-    dir.create(resloc2)
-  }
-  
-  
-  resloc<-paste(resloc2,newsite[k],"/",sep="")
-  if(!dir.exists(resloc)){
-    dir.create(resloc)
-  }
-  res<-tail_analysis(mat = input_tailanal, tot_target_sp=tot_target_sp,resloc = resloc, nbin = 2)
+    #-----------------------adding environmental variable in the matrix-----------------------------
+    tempdat<-env_BT_t%>%filter(lonlat%in%xmeta$lonlat)%>%filter(yr%in%rownames(input_tailanal))%>%dplyr::select(yr,t,tmax,tmin)
+    tempdat$tmax_n<- -tempdat$tmax
+    
+    # check if all TRUE
+    all(rownames(input_tailanal)==tempdat$yr)==T
+    
+    input_tailanal$t<-tempdat$t
+    input_tailanal$tmax<-tempdat$tmax
+    input_tailanal$tmin<-tempdat$tmin
+    input_tailanal$tmax_n<-tempdat$tmax_n
+    input_tailanal<-input_tailanal%>%dplyr::select(-yr)
+    
+    saveRDS(input_tailanal,paste(resloc,"input_mat_for_tailanal_with_env.RDS",sep="")) # dataframe with species timeseries along column
+    
+    #----------- tail analysis ----------------
+    resloc2<-paste("../Results/for_BioTIME/Terrestrial_plotlevel/",site,"/",sep="")
+    if(!dir.exists(resloc2)){
+      dir.create(resloc2)
+    }
+    if(length(newsite)>1){
+      resloc<-paste(resloc2,newsite[k],"/",sep="")
+    }else{
+      resloc<-resloc2
+    }
+    
+    if(!dir.exists(resloc)){
+      dir.create(resloc)
+    }
+    
+    res<-tail_analysis(mat = input_tailanal, tot_target_sp=tot_target_sp, resloc = resloc, nbin = 2)
+    
+    cat("---------- k = ",k,"-----------\n")
   }else{
     cat("---------- number of years consistent with env stats is not sufficient -----------\n")
     newsite_bad<-c(newsite_bad,newsite)
   }
 }
 
-#--------------------------------------------------------
 newsite<-setdiff(newsite,newsite_bad)
 saveRDS(newsite,"../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/420/newsite.RDS")
