161c80fcceed6ac9163724ddc86355cd52423f00
sghosh89 <ghoshshyamolina89@gmail.com>
2022-07-22 17:00:03 +0200

    extracting monthly timeseries for given lonlat

--- DATA/CHELSA_v2/monthly/tasmin/extract_data_for_unique_lonlat_for_BioTIME.R
+++ DATA/CHELSA_v2/monthly/tasmin/extract_data_for_unique_lonlat_for_BioTIME.R
@@ -2,12 +2,13 @@
 # This file has to be in the same folder where you save the environmental data 
 # that you will extract
 ###############################################################################################################
-rm(list=ls())
+path<-dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(path)
 library(raster)
 library(sp)
 rastlist <- list.files(path = "./", pattern='.tif$', all.files=TRUE, full.names=FALSE)
 allrasters <- raster::stack(rastlist)
-df_lonlat<-read.csv("../unique_lonlat_BioTIME.csv")
+df_lonlat<-read.csv("./../../../Results/unique_lonlat_BioTIME.csv")
 #rownames(df_lonlat)<-paste("lon_",df_lonlat$CENT_LONG,"_lat_",df_lonlat$CENT_LAT,sep="")
 
 #df_lonlat<-df_lonlat[7,]
@@ -26,6 +27,5 @@ cnm<-colnames(df_env)
 cnm<-substr(cnm,8,21)
 colnames(df_env)<-cnm
 df_env <- cbind(df_lonlat_table,df_env)
-write.csv(df_env,"../tasmin_monthlyvalues_extracted_lonlat_BioTIME.csv",row.names = F)
-
+write.csv(df_env,"../../../../DATA/wrangled_env_data/tasmin_monthlyvalues_extracted_lonlat_BioTIME.csv",row.names = F)
 
