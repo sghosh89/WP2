312c825dbfe39c62d3799421e14c93b6396efbb9
sghosh89 <ghoshshyamolina89@gmail.com>
2022-07-22 16:45:55 +0200

    extracting monthly timeseries for given lonlat

--- DATA/CHELSA_v2/monthly/tas/extract_data_for_unique_lonlat.R
+++ DATA/CHELSA_v2/monthly/tas/extract_data_for_unique_lonlat.R
@@ -2,12 +2,13 @@
 # This file has to be in the same folder where you save the environmental data 
 # that you will extract
 ###############################################################################################################
-rm(list=ls())
+path<-dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(path)
 library(raster)
 library(sp)
 rastlist <- list.files(path = "./", pattern='.tif$', all.files=TRUE, full.names=FALSE)
 allrasters <- raster::stack(rastlist)
-df_lonlat<-read.csv("../unique_lonlat.csv")
+df_lonlat<-read.csv("../../../../Results/unique_lonlat.csv")
 #rownames(df_lonlat)<-paste("lon_",df_lonlat$CENT_LONG,"_lat_",df_lonlat$CENT_LAT,sep="")
 
 #df_lonlat<-df_lonlat[7,]
@@ -26,6 +27,6 @@ cnm<-colnames(df_env)
 cnm<-substr(cnm,12,18)
 colnames(df_env)<-paste("tas_",cnm,sep="") # don't start column names with number, otherwise will add X in reading csv files
 df_env <- cbind(df_lonlat_table,df_env)
-write.csv(df_env,"../tas_monthlyvalues_extracted_lonlat.csv",row.names = F)
+write.csv(df_env,"../../../../DATA/wrangled_env_data/tas_monthlyvalues_extracted_lonlat.csv",row.names = F)
 
 
