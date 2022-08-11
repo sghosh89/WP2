9e6102c5d43ab78e772c009a5a99f92dbba32471
sghosh89 <ghoshshyamolina89@gmail.com>
2022-08-11 15:33:51 +0200

    adding correlation value

--- R/get_sp_birds.R
+++ R/get_sp_birds.R
@@ -105,31 +105,30 @@ which(df$spearcor_with_t==0)
 
 # so, we will do for all data
 cons_tab_all<-df%>%group_by(spname)%>%
-  summarise(poscor=sum(spearcor_with_t>0),
-            negcor=sum(spearcor_with_t<0),
-            zerocor=sum(spearcor_with_t==0))%>%ungroup()%>%
-group_by(spname)%>%mutate(occur_sites=sum(poscor,negcor,zerocor))%>%
-  mutate(percent_poscor=(poscor/occur_sites)*100,
-         percent_negcor=(negcor/occur_sites)*100,
-         percent_zerocor=(zerocor/occur_sites)*100)%>%ungroup()
-cons_tab_all$diffcor<-cons_tab_all$percent_poscor - cons_tab_all$percent_negcor
+  summarise(poscorsite=sum(spearcor_with_t>0),
+            negcorsite=sum(spearcor_with_t<0),
+            zerocorsite=sum(spearcor_with_t==0),
+            poscorval=sum(spearcor_with_t[which(spearcor_with_t>0)]),
+            negcorval=sum(spearcor_with_t[which(spearcor_with_t<0)]),
+            zerocorval=sum(spearcor_with_t[which(spearcor_with_t==0)]))%>%ungroup()%>%
+  group_by(spname)%>%mutate(occur_sites=sum(poscorsite,negcorsite,zerocorsite))%>%
+  mutate(percent_poscorval=(poscorval/occur_sites)*100,
+         percent_negcorval=(negcorval/occur_sites)*100,
+         percent_zerocorval=(zerocorval/occur_sites)*100)%>%ungroup()
 
-hist(cons_tab_all$diffcor,100,col="green",border=F,
-     xlab="sites(pos.cor - neg.cor)",
-     main=paste("birds, n =",nrow(cons_tab_all)," unique species", sep=""))
+cons_tab_all$allcorval<-cons_tab_all$percent_poscorval + cons_tab_all$percent_negcorval
+rg<-range(cons_tab_all$allcorval)
+rg1<-floor(rg[1])
+rg2<-ceiling(rg[2])
+
+hist(cons_tab_all$allcorval,100,col="green",border=F,
+     xlab="overall corval % across occurence sites",xlim=c(rg1,rg2),
+     main=paste("birds, ",nrow(cons_tab_all)," unique species across ",length(unique(df$newsite))," sites",sep=""))
 abline(v=0,col="black",lty=2)
-sum(cons_tab_all$diffcor>0)/nrow(cons_tab_all)# 64% with positive correlation
-sum(cons_tab_all$diffcor<0)/nrow(cons_tab_all)# 30% sites with negative correlation
-sum(cons_tab_all$diffcor==0)/nrow(cons_tab_all)# 6% sites with no correlation
-text(x=20, y=40, round(100*sum(cons_tab_all$diffcor>0)/nrow(cons_tab_all),2),
+text(x=20, y=25, paste(round(100*sum(cons_tab_all$allcorval>0)/nrow(cons_tab_all),2),"%"),
      col = "red", srt = 0)
-text(x=-20, y=40, round(100*sum(cons_tab_all$diffcor<0)/nrow(cons_tab_all),2),
+text(x=-20, y=25, paste(round(100*sum(cons_tab_all$allcorval<0)/nrow(cons_tab_all),2),"%"),
      col = "blue", srt = 0)
-
-#hist(cons_tab_all$percent_poscor,100,border=F,col=rgb(1,0,0,0.25),
-#     xlab="consistent % of positive and negative cor",
-#     main=paste("birds, n =",nrow(cons_tab_all)))
-#hist(cons_tab_all$percent_negcor,100,border=F,col=rgb(0,0,1,0.25),add=T)
 
 #------------------------------------------
 # now, make histogram for those species which occur in lowT_taxa
@@ -143,46 +142,58 @@ df_highT_taxa<-df%>%filter(newsite%in%highT_taxa$newsite)
 
 # first with lowT 
 cons_tab_lowT_taxa<-df_lowT_taxa%>%group_by(spname)%>%
-  summarise(poscor=sum(spearcor_with_t>0),
-            negcor=sum(spearcor_with_t<0),
-            zerocor=sum(spearcor_with_t==0))%>%ungroup()%>%
-  group_by(spname)%>%mutate(occur_sites=sum(poscor,negcor,zerocor))%>%
-  mutate(percent_poscor=(poscor/occur_sites)*100,
-         percent_negcor=(negcor/occur_sites)*100,
-         percent_zerocor=(zerocor/occur_sites)*100)%>%ungroup()
+  summarise(poscorsite=sum(spearcor_with_t>0),
+            negcorsite=sum(spearcor_with_t<0),
+            zerocorsite=sum(spearcor_with_t==0),
+            poscorval=sum(spearcor_with_t[which(spearcor_with_t>0)]),
+            negcorval=sum(spearcor_with_t[which(spearcor_with_t<0)]),
+            zerocorval=sum(spearcor_with_t[which(spearcor_with_t==0)]))%>%ungroup()%>%
+  group_by(spname)%>%mutate(occur_sites=sum(poscorsite,negcorsite,zerocorsite))%>%
+  mutate(percent_poscorval=(poscorval/occur_sites)*100,
+         percent_negcorval=(negcorval/occur_sites)*100,
+         percent_zerocorval=(zerocorval/occur_sites)*100)%>%ungroup()
 
-cons_tab_lowT_taxa$diffcor<-cons_tab_lowT_taxa$percent_poscor - cons_tab_lowT_taxa$percent_negcor
+cons_tab_lowT_taxa$allcorval<-cons_tab_lowT_taxa$percent_poscorval + cons_tab_lowT_taxa$percent_negcorval
 
-hist(cons_tab_lowT_taxa$diffcor,100,col="green",border=F,
-     xlab="sites(pos.cor - neg.cor) with low T, <50% CI of t_med",
+hist(cons_tab_lowT_taxa$allcorval,100,col="green",border=F,
+     xlab="overall corval % across occurence sites",xlim=c(rg1,rg2),
      main=paste("birds, ",nrow(cons_tab_lowT_taxa)," unique species across ",length(unique(df_lowT_taxa$newsite))," sites",sep=""))
 abline(v=0,col="black",lty=2)
-text(x=30, y=15, paste(round(100*sum(cons_tab_lowT_taxa$diffcor>0)/nrow(cons_tab_lowT_taxa),2),"%"),
+text(x=20, y=20, paste(round(100*sum(cons_tab_lowT_taxa$allcorval>0)/nrow(cons_tab_lowT_taxa),2),"%"),
      col = "red", srt = 0)
-text(x=-30, y=15, paste(round(100*sum(cons_tab_lowT_taxa$diffcor<0)/nrow(cons_tab_lowT_taxa),2),"%"),
+text(x=-20, y=20, paste(round(100*sum(cons_tab_lowT_taxa$allcorval<0)/nrow(cons_tab_lowT_taxa),2),"%"),
      col = "blue", srt = 0)
 
+#sum(cons_tab_lowT_taxa$diffcor==0)
+# 8 sp. show positive response to warming, 2 was independent
 
 # then with highT 
 cons_tab_highT_taxa<-df_highT_taxa%>%group_by(spname)%>%
-  summarise(poscor=sum(spearcor_with_t>0),
-            negcor=sum(spearcor_with_t<0),
-            zerocor=sum(spearcor_with_t==0))%>%ungroup()%>%
-  group_by(spname)%>%mutate(occur_sites=sum(poscor,negcor,zerocor))%>%
-  mutate(percent_poscor=(poscor/occur_sites)*100,
-         percent_negcor=(negcor/occur_sites)*100,
-         percent_zerocor=(zerocor/occur_sites)*100)%>%ungroup()
+  summarise(poscorsite=sum(spearcor_with_t>0),
+            negcorsite=sum(spearcor_with_t<0),
+            zerocorsite=sum(spearcor_with_t==0),
+            poscorval=sum(spearcor_with_t[which(spearcor_with_t>0)]),
+            negcorval=sum(spearcor_with_t[which(spearcor_with_t<0)]),
+            zerocorval=sum(spearcor_with_t[which(spearcor_with_t==0)]))%>%ungroup()%>%
+  group_by(spname)%>%mutate(occur_sites=sum(poscorsite,negcorsite,zerocorsite))%>%
+  mutate(percent_poscorval=(poscorval/occur_sites)*100,
+         percent_negcorval=(negcorval/occur_sites)*100,
+         percent_zerocorval=(zerocorval/occur_sites)*100)%>%ungroup()
 
-cons_tab_highT_taxa$diffcor<-cons_tab_highT_taxa$percent_poscor - cons_tab_highT_taxa$percent_negcor
+cons_tab_highT_taxa$allcorval<-cons_tab_highT_taxa$percent_poscorval + cons_tab_highT_taxa$percent_negcorval
 
-hist(cons_tab_highT_taxa$diffcor,100,col="green",border=F,
-     xlab="sites(pos.cor - neg.cor) with high T, >50% CI of t_med",
+hist(cons_tab_highT_taxa$allcorval,100,col="green",border=F,
+     xlab="overall corval % across occurence sites",xlim=c(rg1,rg2),
      main=paste("birds, ",nrow(cons_tab_highT_taxa)," unique species across ",length(unique(df_highT_taxa$newsite))," sites",sep=""))
 abline(v=0,col="black",lty=2)
-text(x=30, y=15, paste(round(100*sum(cons_tab_highT_taxa$diffcor>0)/nrow(cons_tab_highT_taxa),2),"%"),
+text(x=20, y=10, paste(round(100*sum(cons_tab_highT_taxa$allcorval>0)/nrow(cons_tab_highT_taxa),2),"%"),
      col = "red", srt = 0)
-text(x=-30, y=15, paste(round(100*sum(cons_tab_highT_taxa$diffcor<0)/nrow(cons_tab_highT_taxa),2),"%"),
+text(x=-20, y=10, paste(round(100*sum(cons_tab_highT_taxa$allcorval<0)/nrow(cons_tab_highT_taxa),2),"%"),
      col = "blue", srt = 0)
+
+#--------------------------------------------
+
+
 
 #--------------------------------------------
 
